b57f3498c5071e494ce9836d47ac6c52eea91b6e
+ <+>
2024-07-10 12:16:32 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(a, b) {
+    a = 2000;
+    b = 100;
+    return a / b;
+}
+
+function square(x) {
+    x = 2;
+    return x ** 2;
+}
+
+function add(a, b) {
+    a = 3;
+    b = 4;
+    return a+b;
+}
